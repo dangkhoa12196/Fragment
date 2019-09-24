b7131b9ea4d1b674a396c65635d9cf15997b4b00
dangkhoa12196 <hodangkhoa427@gmail.com>
2019-09-24 15:17:04 +0700

    first commit

---
+++ MyApplication3/app/src/main/java/com/example/student/myapplication/FragmentA.java
@@ -0,0 +1,18 @@
+package com.example.student.myapplication;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentA extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View v=inflater.inflate(R.layout.fragment_a,container,false);
+        return v;
+    }
+}
---
+++ MyApplication3/app/src/main/java/com/example/student/myapplication/FragmentB.java
@@ -0,0 +1,17 @@
+package com.example.student.myapplication;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentB extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View v=inflater.inflate(R.layout.fragment_b,container,false);
+        return v;
+    }
+}
